85f955f7c9d47a2aaf3a4961f3093dbed64d3a2e
natankw <hll931200@gmail.com>
2026-07-23 14:37:08 -0300

    Create api.ts

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { useAuthStore } from '../store/auth.store';
+
+const api = axios.create({
+  baseURL: '/api',
+  headers: { 'Content-Type': 'application/json' },
+});
+
+api.interceptors.request.use((config) => {
+  const { token } = useAuthStore.getState();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
